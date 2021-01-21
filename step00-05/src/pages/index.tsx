f0c992935fc559af2076b2496e98353576fc2d74
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-21 16:55:14 +0500

    Step 01
    1:- Create
    	a:- src\pages\about.tsx to define about page
    2:- update
    	b:- src\pages\index.tsx to add link to about page in the app

--- step00-05/src/pages/index.tsx
+++ step00-05/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from "gatsby";
 
 export default function home() {
   return (
   <div> 
-      Hello world
+      <p>We can link pages in Gatsby by using Link</p>
+      <Link to="/about/">About</Link>
   </div>
   )
 }
